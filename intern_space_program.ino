ccf022995510c8c5cc31516af81989d364272a2d
Connor Jakubik <ducttapeismagic@tamu.edu>
2018-07-28 19:48:47 -0500

    Better comments for loop and logic for state switch on 0-2

--- intern_space_program.ino
+++ intern_space_program.ino
@@ -6,10 +6,20 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 
 #define ADC_REF 5 //5v
+
+// PINS
 #define ROTARY_ANGLE_SENSOR A0 //Analog pin 0
-#define GROVE_VCC 5 //VCC of the grove interface is normally 5v
+#define BOOSTER_IIST        A6 //TODO update to reflect actual pin
+
+// STATE THRESHOLD PARAMETERS //TODO tune all
+#define LAUNCH_ACCEL     980.0 // m/s^2, the forward acceleration at which launch definitely happened
+#define SEP_ACCEL       1200.0 // m/s^2, the forward acceleration at which separation definitely happened
+#define MIN_SEP_ALTITUDE 152.4 // meters, 500 ft, minumum altitude at which separation would possibly happen
+
+// OTHER
+#define GROVE_VCC  5 //VCC of the grove interface is normally 5v
 #define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
-#define LOCK 1 //mutux lock
+#define LOCK   1 //mutux lock
 #define UNLOCK 0 //mutux unlock
 
 const int colorR = 255;    //red
@@ -19,17 +29,24 @@ const int colorB = 0;      //blue
 
 Pixy pixy; // This is the main Pixy object
 
-int servoLeftPin = 9;      //digital pin 9
+// IIST Sensors
+bool boosterIIST = true;
+
+// SERVOS
+
+const int servoLeftPin = 9;      //digital pin 9
 Servo servoLeft;          //create servo
 int servoLeftAngle = 0;   // servo position in degrees
 
-int servoRightPin = 10;   //digital pin 10
+const int servoRightPin = 10;   //digital pin 10
 Servo servoRight;          //create servo
 int servoRightAngle = 0;    //servo position in degrees
 
-int servoBackPin = 11;   //digital pin 10
+const int servoBackPin = 11;   //digital pin 10
 Servo servoBack;          //create servo
 int servoBackAngle = 0;    //servo position in degrees
+
+// MPU
 
 MPU6050 mpu; //mpu object
 bool dmpReady = false;  // set true if DMP init was successful
@@ -81,57 +98,77 @@ void loop() {
   //6 ORBIT
   //7 RECOVER_ABORT
   //8 LANDED
-    case 0:
+    case 0: //0 SCRUB
+      if (AUTOSTATE){
+        // SCRUB never automatically changes to another state
+        // the Diagnostics() function has authority to change state to GROUND_READY
+      }
+      break;
+    case 1: //1 GROUND_READY
+      if (AUTOSTATE){
+
+        // Sense Launch Sensor Fusion
+        if(false/*accelForward>LAUNCH_ACCEL && altitude>ALTIMETER_UNCERTAINTY*/){ //TODO
+            STATE = 2; //BOOST
+
+        }
+      }
+      break;
+    case 2: //2 BOOST
+      if (AUTOSTATE){
+        // IIST sensor
+        boosterIIST = digitalRead(BOOSTER_IIST);
+
+        // Sense Ejection Sensor Fusion
+        if(abs(accelForward)>SEP_ACCEL && altitude>MIN_SEP_ALTITUDE){
+            if(boosterIIST)
+            {
+                STATE = 3; // SEPARATE
+            }
+        }
+
+        // Timer Watchdog
+        //TODO make this
+
+      }
+      break;
+    case 3: //3 SEPARATE
       if (AUTOSTATE){
         //make state change
       }
       break;
-    case 1:
+    case 4: //4 STABILIZE
       if (AUTOSTATE){
         //make state change
       }
       break;
-    case 2:
+    case 5: //5 SEARCH
       if (AUTOSTATE){
         //make state change
       }
       break;
-    case 3:
+    case 6: //6 ORBIT
       if (AUTOSTATE){
         //make state change
       }
       break;
-    case 4:
+    case 7: //7 RECOVER_ABORT
       if (AUTOSTATE){
         //make state change
       }
       break;
-    case 5:
-      if (AUTOSTATE){
-        //make state change
-      }
-      break;
-    case 6:
-      if (AUTOSTATE){
-        //make state change
-      }
-      break;
-    case 7:
-      if (AUTOSTATE){
-        //make state change
-      }
-      break;
-    case 8:
+    case 8: //8 LANDED
       if (AUTOSTATE){
         //make state change
       }
       break;
     default:
+
   }
 }
 
 void mutux(int lock){
-  //used for interrupt-dependent functions
+  // Used to change whether or not to allow interrupts
   if (lock == 1){
     noInterrupts();
   }
@@ -146,6 +183,7 @@ void pi_go(){
     while (!Serial.available());                 // wait for data
     while (Serial.available() && Serial.read()); // empty buffer again
 }
+
 void initialize(){
   //    MPU setup
   //    Pixy Setup
