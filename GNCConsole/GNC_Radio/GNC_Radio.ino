4302f14d2d330594aa3e05aafbee70b845aa3e15
Gabe R <Lockheedmaniac@gmail.com>
2018-07-29 20:17:27 -0500

    Begin testing GNC Radio System

--- GNCConsole/GNC_Radio/GNC_Radio.ino
+++ GNCConsole/GNC_Radio/GNC_Radio.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+// Arduino9x_RX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (receiver)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Arduino9x_TX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 16
+#define RFM95_RST 5
+#define RFM95_INT 4
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+
+  Serial.println("Arduino LoRa RX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  //delibrate break of stuff to 
+void loop()
+{
+  print('hi');
 }
