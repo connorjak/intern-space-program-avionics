a7fbba88a7ac9b1574f07a70b52c963230b1e146
Ronnie Ankner <ronnieankner@gmail.com>
2018-08-03 12:38:37 -0500

    small change

--- flightcomputer/flightcomputer.ino
+++ flightcomputer/flightcomputer.ino
@@ -153,6 +153,14 @@ uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 volatile bool mpuInterruptFlag = false;  // indicates whether MPU interrupt pin has gone high
 
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
 
 // *** TRX ***
 
